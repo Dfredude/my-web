1c2c0a221486e15167b72c298d85864968de67e1
Alfredo Dominguez <thecatura.ad@gmail.com>
2024-04-17 09:40:24 -0300

    Production issues solved

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
 const env = {
     API_DEVELOPMENT_HOST: "http://localhost:8080",
-    API_PRODUCTION_HOST: "http://cv-answerer-bpwjfipn5a-uc.a.run.app",
+    API_PRODUCTION_HOST: "https://cv-answerer-bpwjfipn5a-uc.a.run.app",
     PORT: 8080
 }
