b589604c1388ab973b76573695487791e0f7026d
Alfredo Dominguez <thecatura.ad@gmail.com>
2024-04-17 10:00:44 -0300

    Added loading spinner

--- main.js
+++ main.js
@@ -10,8 +10,9 @@ function main() {
     const REST_API_host = env.API_PRODUCTION_HOST;
     // const REST_API_host = env.API_PRODUCTION_HOST;
     document.getElementById("ask-btn").addEventListener("click", function() {
+        // Set loading spinner
+        document.getElementById("answer-loading-spinner").classList.remove("d-none");
         const question = document.getElementById("question");
-        console.log(question);
         fetch(REST_API_host + "/about?" + new URLSearchParams({
             question: question.value
         }))
@@ -19,12 +20,13 @@ function main() {
             return response.text()
         })
         .then(answer => {
-            console.log(answer);
             document.getElementById("answer").innerText = answer
+            document.getElementById("answer-loading-spinner").classList.add("d-none");
         }
         )
         .catch(error => {
             console.error("Error:", error);
+            document.getElementById("answer").innerText = "Error: " + error;
         });
     });
 }
