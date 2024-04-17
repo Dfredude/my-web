3fab64b6654b1594ce944c7ff502c2066fc75257
Alfredo Dominguez <thecatura.ad@gmail.com>
2024-04-17 09:26:35 -0300

    Updated environment vars

---
+++ env.js
@@ -0,0 +1,5 @@
+const env = {
+    API_DEVELOPMENT_HOST: "http://localhost:8080",
+    API_PRODUCTION_HOST: "http://cv-answerer-bpwjfipn5a-uc.a.run.app",
+    PORT: 8080
+}
--- main.js
+++ main.js
@@ -1,20 +1,31 @@
-const REST_API_host = "https://cv-answerer-bpwjfipn5a-uc.a.run.app"
+require(['../env'], function(env) {
+    console.log("Loaded env.js");
+    main(env)
+})
 
-document.getElementById("ask-btn").addEventListener("click", function() {
-    const question = document.getElementById("question");
-    console.log(question);
-    fetch(REST_API_host + "/about?" + new URLSearchParams({
-        question: question.value
-    }))
-    .then(response => {
-        return response.text()
-    })
-    .then(answer => {
-        console.log(answer);
-        document.getElementById("answer").innerText = answer
-    }
-    )
-    .catch(error => {
-        console.error("Error:", error);
+
+// const REST_API_host = "localhost"
+
+function main() {
+    const REST_API_host = env.API_PRODUCTION_HOST;
+    // const REST_API_host = env.API_PRODUCTION_HOST;
+    document.getElementById("ask-btn").addEventListener("click", function() {
+        const question = document.getElementById("question");
+        console.log(question);
+        fetch(REST_API_host + "/about?" + new URLSearchParams({
+            question: question.value
+        }))
+        .then(response => {
+            return response.text()
+        })
+        .then(answer => {
+            console.log(answer);
+            document.getElementById("answer").innerText = answer
+        }
+        )
+        .catch(error => {
+            console.error("Error:", error);
+        });
     });
-});
+}
+
